013327a6a1a2b73dc48c1d782881b760ea19d2c0
Stephen Peters <7speter@gmail.com>
2020-05-01 03:34:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable.
   # return the entire updated hash
-	alans_new_info = "Dunked on MK"
+	#alans_new_info = "Dunked on MK"
 	programmer_hash =
  		{
       :grace_hopper => {
